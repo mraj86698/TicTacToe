609a354a5cb57bbbb17e432c1c59ed15ef28e439
MohanRaj <mraj86698@gmail.com>
2022-08-22 08:34:43 +0530

    Added to Choose a Letter for X or O

--- src/TicTac/TicTac.java
+++ src/TicTac/TicTac.java
@@ -19,5 +19,34 @@ public class TicTac {
 		}
 		return board;
 	}
+	/**
+	 * Determine Player & Computer to Play the Game.
+	 * It takes input from the user.
+	 * if user letter is 'X' then computer letter is become 'O' otherwise it becomes 'X'
+	 * @return X & O
+	 */
+
+	public char[] getInput() {
+		char x='x';
+		char o='o';
+		char[] output=new char[2];
+		System.out.println("Please enter a character X or O");
+		String input=(sc.next()).toLowerCase();
+		char mark=input.charAt(0);
+		if (mark==x) {
+			System.out.println("User has entered: "+x);
+			output[0]=x;
+			System.out.println("Computer has entered:  "+o);
+			output[1]=o;
+		}else if(mark==o) {
+			System.out.println("User has entered: "+o);
+			output[0]=o;
+			System.out.println("Computer has entered: "+x);
+			output[1]=x;
+		}else {
+			System.out.println("Unknown entry.");
+		}
+		return output;
+	}
 
 }
